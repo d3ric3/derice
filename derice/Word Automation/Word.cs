5a07bd8ced67309dff3cbdbfdda8e3ca5f5b890a
d3ric3 <d3ric3@gmail.com>
2013-02-08 16:43:38 +0800

    Added extract object properties

--- derice/Word Automation/Word.cs
+++ derice/Word Automation/Word.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -11,9 +12,9 @@ namespace derice.office
         public Word(string path) : base(path) { }
         public Word(byte[] content) : base(content) { }
 
-        protected string[] ExtractMacroButton()
+        protected Dictionary<string,string> ExtractMacroButton()
         {
-            List<string> keys = new List<string>();
+            Dictionary<string, string> rtnValues = new Dictionary<string, string>();
 
             //Retrieve all occurance in the format of {MACROBUTTON NoMacro [keyword]}
             foreach (Match m in Regex.Matches(this.XML_Content, @"\{\s{0,}macrobutton\s{1,}nomacro\s{1,}\[[_\w\d]+\]\s{0,}\}", RegexOptions.IgnoreCase))
@@ -21,10 +22,56 @@ namespace derice.office
                 //retrieve value between [ to ]
                 int startIndexOfKey = m.ToString().IndexOf('[') + 1;
                 int lengthOfKey = m.ToString().IndexOf(']') - m.ToString().IndexOf('[') - 1;
+
                 string key = m.ToString().Substring(startIndexOfKey, lengthOfKey);
+                string value = m.ToString();
+                rtnValues.Add(key, value);
             }
 
-            return keys.ToArray();
+            return rtnValues;
+        }
+
+        protected Dictionary<string,string> ExtractObjectProperties(Object obj, string DateTimeFormat = "dd/MM/yyyy")
+        {
+            Dictionary<string, string> rtnValues = new Dictionary<string, string>();
+            
+            foreach (PropertyInfo pi in obj.GetType().GetProperties())
+            {
+                if (pi.PropertyType.Name.Contains("List") || pi.PropertyType.Name.Contains(@"[]"))
+                {
+                    continue;
+                }
+                else
+                {
+                    if (pi.PropertyType.Name == "DateTime")
+                    {
+                        string key = pi.Name;
+                        string value = ((DateTime)pi.GetValue(obj, null)).ToString(DateTimeFormat);
+                        rtnValues.Add(key, value);                        
+                    }
+                    else
+                    {
+                        string key = pi.Name;
+                        string value = pi.GetValue(obj, null).ToString();
+                        rtnValues.Add(key, value);
+                    }
+                }
+                //Console.WriteLine(string.Format("Name: {0}, Type: {1}, Value: {2}", pi.Name, pi.PropertyType.Name, pi.GetValue(obj, null)));
+            }
+
+            return rtnValues;
+        }
+
+        protected List<Dictionary<string, string>> ExtractListObjectProperties(List<object> objects, string DateTimeFormat = "dd/MM/yyyy")
+        {
+            List<Dictionary<string, string>> rtnValues = new List<Dictionary<string, string>>();
+
+            foreach (var obj in objects)
+            {
+                rtnValues.Add(ExtractObjectProperties(obj, DateTimeFormat));
+            }
+
+            return rtnValues;
         }
     }
 }
--- derice/test/Program.cs
+++ derice/test/Program.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -12,11 +14,87 @@ namespace test
     {
         static void Main(string[] args)
         {
-            DateTime dt = new DateTime(2013, 2, 1);
-            dt = dt.AddDays(60);
+            Person person = new Person();
+            person.IC = "860128145103";
+            person.ContactNumber = 0176631034;
+            person.DOB = new DateTime(1986, 1, 28);
+            person.Name = "d3ric3";
 
-            Console.WriteLine(DateTime.Today.AddDays(-30).ToString("dd-MMM-yyyy"));
+            Person person1 = new Person();
+            person1.IC = "86012814510322";
+            person1.ContactNumber = 017663103422;
+            person1.DOB = new DateTime(1986, 1, 28);
+            person1.Name = "d3ric322";
+
+            List<Person> ppl = new List<Person>();
+            ppl.Add(person);
+            ppl.Add(person1);
+
+            ExtractObjectProperties(person, "dd MMM yyyy");
+            //PrintListObjectProperties(ConvertToObjectList(ppl));
+
             Console.Read();
         }
+
+        protected static Dictionary<string, string> ExtractObjectProperties(Object obj, string DateTimeFormat = "dd/MM/yyyy")
+        {
+            Dictionary<string, string> rtnValues = new Dictionary<string, string>();
+
+            foreach (PropertyInfo pi in obj.GetType().GetProperties())
+            {
+                if (pi.PropertyType.Name.Contains("List") || pi.PropertyType.Name.Contains(@"[]"))
+                {
+                    continue;
+                }
+                else
+                {
+                    if (pi.PropertyType.Name == "DateTime")
+                    {
+                        string key = pi.Name;
+                        string value = ((DateTime)pi.GetValue(obj, null)).ToString(DateTimeFormat);
+                        rtnValues.Add(key, value);
+                        Console.WriteLine(string.Format("Name: {0}, Type: {1}, Value: {2}", key, pi.PropertyType.Name, value));
+                    }
+                    else
+                    {
+                        string key = pi.Name;
+                        string value = pi.GetValue(obj, null).ToString();
+                        rtnValues.Add(key, value);
+                        Console.WriteLine(string.Format("Name: {0}, Type: {1}, Value: {2}", key, pi.PropertyType.Name, value));
+                    }
+                }                
+            }
+
+            return rtnValues;
+        }
+
+        protected static void PrintListObjectProperties(List<Object> objs)
+        {
+            foreach (var obj in objs)
+            {
+                ExtractObjectProperties(obj);
+            }
+        }
+
+        protected static List<Object> ConvertToObjectList(IEnumerable<Object> objs)
+        {
+            List<object> value = new List<object>();
+            foreach (var obj in objs)
+            {
+                value.Add(obj);
+            }
+            return value;
+        }
+    }
+
+
+    public class Person
+    {
+        public string IC { get; set; }
+        public Int64 ContactNumber { get; set; }
+        public DateTime DOB { get; set; }
+        public string Name { get; set; }
+        public List<Person> Buddies { get; set; }
+        public Person[] MyProperty { get; set; }
     }
 }
